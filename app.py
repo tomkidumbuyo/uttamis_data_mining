113bdd0f6772dc170f3306c1f495d89c2cba8379
thomas <mryoungtommy@gmail.com>
2021-08-24 00:53:27 +0300

    removed empty \n (lines) from and triming the output

--- app.py
+++ app.py
@@ -8,19 +8,24 @@ outfile = open("table_data.csv","w",newline='')
 writer = csv.writer(outfile)
 tab_data = []
 
-for x in range(2667):
-
-    page = requests.get(URL + str(x+1))
+for x in range(2668):
+    print(x)
+    page = requests.get(URL + str(x))
     tree = BeautifulSoup(page.content,"html.parser")
     table_tag = tree.select("table")[0]
     if x == 0:
-        tab_data = [[item.text for item in row_data.select("th,td")]
+        tab_data = [[item.text.strip() for item in row_data.select("th,td")]
                         for row_data in table_tag.select("tr")]
     else:
         tab_data += [[item.text for item in row_data.select("td")]
                         for row_data in table_tag.select("tr")]
+
+    
    
 
 for data in tab_data:
-    writer.writerow(data)
-    print(' '.join(data))
+    if len(data):
+        data = list(map(lambda x: x.strip().replace("\n", ""), data))
+        writer.writerow(data)
+        print(data)
+        #print(' '.join(data))
